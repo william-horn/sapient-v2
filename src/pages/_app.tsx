0b4bc8164ee8cdda1c1d31ad93a7581de430ab89
William Horn <williamjosephhorn@gmail.com>
2023-01-25 20:31:07 -0500

    attempting mongoose bug fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import AppProvider from 'providers/AppProvider'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import AppProvider from 'providers/AppProvider';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
